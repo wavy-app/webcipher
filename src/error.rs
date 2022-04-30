83499db304d9814d7c370942d119559c6d0ac214
Raunak Bhagat <rabhagat31@gmail.com>
2022-04-30 14:28:40 -0600

    Initial commit

---
+++ src/error.rs
@@ -0,0 +1,42 @@
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+pub enum Error {
+    invalid_uri,
+    unable_to_fetch_keys { message: String },
+    unrecognized_response { message: String },
+    unable_to_verify_token { message: String },
+    invalid_token,
+    unrecognized_jws_type,
+    no_kid_present,
+    no_corresponding_kid_in_store,
+}
+
+impl From<hyper::Error> for Error {
+    fn from(e: hyper::Error) -> Self {
+        Self::unable_to_fetch_keys {
+            message: e.to_string(),
+        }
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(e: serde_json::Error) -> Self {
+        Self::unrecognized_response {
+            message: e.to_string(),
+        }
+    }
+}
+
+impl From<http::uri::InvalidUri> for Error {
+    fn from(_: http::uri::InvalidUri) -> Self {
+        Self::invalid_uri
+    }
+}
+
+impl From<jsonwebtoken::errors::Error> for Error {
+    fn from(e: jsonwebtoken::errors::Error) -> Self {
+        Self::unable_to_verify_token {
+            message: e.to_string(),
+        }
+    }
+}
---
+++ src/key.rs
@@ -0,0 +1,17 @@
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Hash, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Key {
+    #[serde(default)]
+    pub e: String,
+
+    pub kty: String,
+
+    pub alg: Option<String>,
+
+    #[serde(default)]
+    pub n: String,
+
+    pub kid: String,
+}
