80a52f4d7b0850d08fd88eb62b16057bf735685f
Raunak Bhagat <rabhagat31@gmail.com>
2022-04-30 20:32:52 -0600

    Add remote and local stores

--- src/key_stores/remote/mod.rs
+++ src/key_stores/remote/mod.rs
@@ -1,5 +1,9 @@
 //! An abstraction over the key management for `JWKs`.
 
+pub mod key;
+#[cfg(test)]
+mod tests;
+
 use std::collections::HashMap;
 use std::collections::HashSet;
 
@@ -15,16 +19,16 @@ use serde::Deserialize;
 use serde_json::Value;
 
 use crate::error::Error;
-use crate::jwk_registry;
-use crate::key::Key;
+use crate::key_stores::remote::key::Key;
+use crate::prelude;
 
 /// A storage location for all `JWK`'s used for encryption for `OAuth2`.
 /// The `URI` of the target is stored and the corresponding keys are fetched
 /// upon creation.
 ///
 /// One can then decode tokens presumably signed by the target by calling
-/// [`decode`](`KeyStore::decode`). If the token was indeed provisioned by the
-/// target, this operation will be successful. Otherwise, it will fail.
+/// [`decode`](`RemoteStore::decode`). If the token was indeed provisioned by
+/// the target, this operation will be successful. Otherwise, it will fail.
 ///
 /// For example, consider a struct, `MyClaims`, which contains the required
 /// public claims, as well as your own specific private claims.
@@ -41,20 +45,20 @@ use crate::key::Key;
 /// }
 ///
 /// let uri = "https://my_target.com/certs";
-/// let key_store = KeyStore::new(uri).await?;
+/// let key_store = RemoteStore::new(uri).await?;
 ///
 /// let token = "a.b.c";
 /// let claims: MyClaims = key_store.decode::<MyClaims>(&token)?;
 /// ```
-/// 
+///
 /// Many targets rotate their keys, and as such, cached keys will fail after a
-/// certain period of time. [`KeyStore`] provides the
-/// [`refresh`](`KeyStore::refresh`) function to refresh a given [`KeyStore`].
-/// [`refresh`](`KeyStore::refresh`) will re-fetch the new keys (from its
-/// current `uri`).
+/// certain period of time. [`RemoteStore`] provides the
+/// [`refresh`](`RemoteStore::refresh`) function to refresh a given
+/// [`RemoteStore`]. [`refresh`](`RemoteStore::refresh`) will re-fetch the new
+/// keys (from its current `uri`).
 #[derive(Derivative)]
 #[derivative(Hash, PartialEq, Eq)]
-pub struct KeyStore {
+pub struct RemoteStore {
     /// The [`URI`] from which to fetch the keys.
     ///
     /// [`URI`]: https://docs.rs/http/latest/http/uri/struct.Uri.html
@@ -66,38 +70,40 @@ pub struct KeyStore {
     /// Since `JWT`'s are signed by a [`Key`] that has a matching `kid`, this
     /// mapping makes it easy to find the corresponding [`Key`].
     ///
-    /// Two [`KeyStore`]'s are considered equivalent if and only if their
+    /// Two [`RemoteStore`]'s are considered equivalent if and only if their
     /// `uri`'s match.
     #[derivative(PartialEq = "ignore", Hash = "ignore")]
-    pub(crate) keys: HashMap<String, Key>,
+    pub(crate) keys: HashMap<String, (Key, DecodingKey)>,
 }
 
-impl KeyStore {
-    /// Generate a new [`KeyStore`] by asynchronously fetching the keys at the
-    /// given [`URI`].
+impl RemoteStore {
+    /// Generate a new [`RemoteStore`] by asynchronously fetching the keys at
+    /// the given [`URI`].
     ///
     /// [`URI`]: https://docs.rs/http/latest/http/uri/struct.Uri.html
-    pub async fn new<I>(uri: I) -> jwk_registry::Result<Self>
+    pub async fn new<I>(uri: I) -> prelude::Result<Self>
     where
         String: From<I>,
     {
         let uri = String::from(uri).parse::<http::Uri>()?;
         let keys = fetch(uri.clone()).await?;
+        let keys = attach_decoding_keys(keys);
 
         let store = Self { uri, keys };
 
         Ok(store)
     }
 
-    /// Refreshes the current [`KeyStore`] by asynchronously fetching the keys
-    /// at the given [`URI`].
+    /// Refreshes the current [`RemoteStore`] by asynchronously fetching the
+    /// keys at the given [`URI`].
     ///
     /// Useful for when targets rotate their keys.
     ///
     /// [`URI`]: https://docs.rs/http/latest/http/uri/struct.Uri.html
-    pub async fn refresh(&mut self) -> jwk_registry::Result<()> {
+    pub async fn refresh(&mut self) -> prelude::Result<()> {
         let Self { uri, .. } = self;
         let keys = fetch(uri.clone()).await?;
+        let keys = attach_decoding_keys(keys);
 
         self.keys = keys;
 
@@ -107,14 +113,14 @@ impl KeyStore {
     pub fn decode<Claim, I>(
         &self,
         token: I,
-    ) -> jwk_registry::Result<TokenData<Claim>>
+    ) -> prelude::Result<TokenData<Claim>>
     where
         String: From<I>,
         Claim: for<'a> Deserialize<'a>,
     {
         let Self { keys, .. } = self;
 
-        let token: String = token.into();
+        let ref token: String = token.into();
         let jsonwebtoken::Header { typ, alg, kid, .. } =
             decode_header(&token).unwrap();
 
@@ -127,13 +133,12 @@ impl KeyStore {
             .ok_or(Error::unrecognized_jws_type)?;
         let kid = kid.ok_or(Error::no_kid_present)?;
 
-        let Key { e, n, .. } =
+        let (_, decoding_key) =
             keys.get(&kid).ok_or(Error::no_corresponding_kid_in_store)?;
 
-        let validation = Validation::new(alg);
-        let key = DecodingKey::from_rsa_components(n, e)?;
+        let ref validation = Validation::new(alg);
 
-        let claim = decode::<Claim>(&token, &key, &validation)?;
+        let claim = decode::<Claim>(token, decoding_key, validation)?;
 
         Ok(claim)
     }
@@ -149,12 +154,12 @@ impl KeyStore {
     }
 
     /// Get an immutable reference to the inner `keys` cache-map.
-    pub fn keys(&self) -> &HashMap<String, Key> {
+    pub fn keys(&self) -> &HashMap<String, (Key, DecodingKey)> {
         &self.keys
     }
 
     /// Get a mutable reference to the inner `keys` cache-map.
-    pub fn keys_mut(&mut self) -> &mut HashMap<String, Key> {
+    pub fn keys_mut(&mut self) -> &mut HashMap<String, (Key, DecodingKey)> {
         &mut self.keys
     }
 }
@@ -165,7 +170,7 @@ impl KeyStore {
 /// Each JWT can be decrypted by a corresponding [`Key`] that has a matching
 /// `kid`.
 /// Therefore, the returned hashmap is indexed as: `kid -> Key`.
-async fn fetch(uri: http::Uri) -> jwk_registry::Result<HashMap<String, Key>> {
+async fn fetch(uri: http::Uri) -> prelude::Result<HashMap<String, Key>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
     let mut response = client.get(uri).await?;
@@ -191,3 +196,24 @@ async fn fetch(uri: http::Uri) -> jwk_registry::Result<HashMap<String, Key>> {
 
     Ok(keys)
 }
+
+fn attach_decoding_keys(
+    key_map: HashMap<String, Key>,
+) -> HashMap<String, (Key, DecodingKey)> {
+    key_map
+        .into_iter()
+        .filter_map(|(kid, key)| {
+            let Key { e, n, .. } = &key;
+            DecodingKey::from_rsa_components(n, e)
+                .ok()
+                .map(|decoding_key| (kid, (key, decoding_key)))
+        })
+        .collect()
+}
+
+// #[test]
+// fn test() {
+//     let token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.b.c";
+//     let x = jsonwebtoken::decode_header(token).unwrap();
+//     println!("{:#?}", x);
+// }
